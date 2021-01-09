9b8e58b6d5b03eccec59fbe1fbfa4e132a5a6ed2
Weston Jones <Weston Jones@SPACESTATION>
2021-01-08 23:51:15 -0700

    Final program for week 1.

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -10,24 +10,44 @@ namespace ConsoleApplication
     {
 
         static void Main(string[] args)
-        {
+        {   //2
             var name = "Weston Jones";
             var location = "Rexburg, ID";
-
+            //3
             Console.WriteLine($"My name is {name}");
             Console.WriteLine($"I am from {location}");
-
+            //4
             var currentDate = DateTime.Now;
             Console.WriteLine("The current date is " + currentDate.ToString("d"));
+            //5 number of days until christmas
+            var day =  (currentDate.DayOfYear - 359) * -1;
+            Console.WriteLine("There are " + day.ToString("d") + " days until Christmas!");
+            
+            //6
+            double width, height, woodLength, glassArea;
+            string widthString, heightString;
 
+            Console.WriteLine("Please input a valid width number");
+            widthString = Console.ReadLine();
+            width = double.Parse(widthString);
+
+            Console.WriteLine("Please input a valid height number");
+            heightString = Console.ReadLine();
+            height = double.Parse(heightString);
+
+            woodLength = 2 * (width + height) * 3.25;
+
+            glassArea = 2 * (width * height);
+
+            Console.WriteLine("The length of the wood is " +
+            woodLength + " feet");
+            Console.WriteLine("The area of the glass is " +
+            glassArea + " square metres");
+
+            //pause the console
             Console.WriteLine("Press any key to exit");
             Console.ReadKey();
 
-
-
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
         }
     }
 }

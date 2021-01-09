e5b3a7817c607b88eb3277f6e1541695600b0727
Weston Jones <Weston Jones@SPACESTATION>
2021-01-08 23:14:11 -0700

    First change

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -6,14 +6,25 @@ using System.Windows.Forms;
 
 namespace ConsoleApplication
 {
-    static class Program
+    class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
+
+        static void Main(string[] args)
         {
+            var name = "Weston Jones";
+            var location = "Rexburg, ID";
+
+            Console.WriteLine($"My name is {name}");
+            Console.WriteLine($"I am from {location}");
+
+            var currentDate = DateTime.Now;
+            Console.WriteLine("The current date is " + currentDate.ToString("d"));
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+
+
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
